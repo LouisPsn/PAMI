f9a12acb16ea04e8ebd995b471350b1a4d996f5c
LouisPsn <lpierson002@enseirb-matmeca.fr>
2025-05-30 16:07:23 +0200

    Working PAMI

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,80 @@
 #include <Arduino.h>
-#include <Dynamixel2Arduino.h>
-#include <SoftwareSerial.h>
+#include <Dynamixel_Serial.h>       // Library needed to control Dynamixal servo
+#include <Servo.h>
 
-/*--------------*/
-/* Left TCS3200 */
-/*--------------*/
-#define OUT_LEFT 2
-#define OE_LEFT 14
-#define S0_LEFT 16
-#define S1_LEFT 15
-#define S2_LEFT 3
-#define S3_LEFT 4
-
-/*---------------*/
-/* Right TCS3200 */
-/*---------------*/
-#define OUT_RIGHT 5
-#define OE_RIGHT 17
-#define S0_RIGHT 19
-#define S1_RIGHT 18
-#define S2_RIGHT 6
-#define S3_RIGHT 7
+Servo servo;  // create servo object to control a servo
 
 /*---------------*/
 /* Right HC-SR04 */
 /*---------------*/
-#define TRIG_MIDDLE 10
-#define ECHO_MIDDLE 11
+#define TRIG_RIGHT 10
+#define ECHO_RIGHT 11
 
 /*--------------*/
 /* Left HC-SR04 */
 /*--------------*/
-#define TRIG_LEFT 12
-#define ECHO_LEFT 13
+#define TRIG_LEFT A6
+#define ECHO_LEFT A7
 
 /*----------------*/
 /* Middle HC-SR04 */
 /*----------------*/
-#define TRIG_RIGHT A6
-#define ECHO_RIGHT A7
+#define TRIG_MIDDLE 12
+#define ECHO_MIDDLE 13
 
 /*---------*/
 /* Tirette */
 /*---------*/
-#define TIRETTE_PIN 9
+#define TIRETTE_PIN 17
 
-/*-----------------*/
-/* Sensor Position */
-/*-----------------*/
-#define LEFT 0
-#define MIDDLE 1
-#define RIGHT 2
+/*--------------*/
+/* Interrupteur */
+/*--------------*/
+#define INTERRUPT_PIN 16
 
-/*-----------------*/
-/* Dynamixel MX-28 */
-/*-----------------*/
-SoftwareSerial soft_serial(1, 0); // DYNAMIXELShield UART RX/TX
-#define DXL_SERIAL   Serial
-#define DEBUG_SERIAL soft_serial
-#define DXL_DIR_PIN 10  // DYNAMIXEL Shield DIR PIN
+/*------*/
+/* LEDS */
+/*------*/
+int redPin = 5;
+int greenPin = 6;
+int bluePin = 8;
 
-const uint8_t DXL_ID_1 = 1;
-const uint8_t DXL_ID_2 = 2;
-const float DXL_PROTOCOL_VERSION = 2.0;
+void move_servo()
+{
+  servo.write(50);
+  delay(15);
+  servo.write(75);
+  delay(15);
+}
 
-Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+void setColor(int redValue, int greenValue,  int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin,  greenValue);
+  analogWrite(bluePin, blueValue);
+}
 
+void light_LEDs() {
+  setColor(255, 0, 0); // Red Color
+  delay(1000);
+  setColor(0,  255, 0); // Green Color
+  delay(1000);
+  setColor(0, 0, 255); // Blue Color
+  delay(1000);
+  setColor(255, 255, 255); // White Color
+  delay(1000);
+  setColor(170, 0, 255); // Purple Color
+  delay(1000);
+  setColor(127, 127,  127); // Light Blue
+  delay(1000);
+}
 
-int RGB_LEFT[3] = {0};
-int RGB_RIGHT[3] = {0};
+/*-------*/
+/* MX-28 */
+/*-------*/
+#define SERVO_ID_1 0x01               // ID of which we will set Dynamixel too
+#define SERVO_ID_2 0x02               // ID of which we will set Dynamixel too
+#define SERVO_ControlPin 0x7        // Control pin of buffer chip, NOTE: this does not matter becasue we are not using a half to full contorl buffer.
+#define SERVO_SET_Baudrate 57600    // Baud rate speed which the Dynamixel will be set too (57600)
 
 int read_tirette()
 {
@@ -123,77 +129,51 @@ float read_right_HC_SR04()
   return distance_right;
 }
 
-void read_left_TCS3200()
+void go_straight_MX_28()
 {
-  // Set sensor to read Red only
-	digitalWrite(S2_LEFT, LOW);
-	digitalWrite(S3_LEFT, LOW);
-	// Read the output Pulse Width
-	RGB_LEFT[0] = pulseIn(OUT_LEFT, LOW);
-	
-
-  // Set sensor to read Green only
-	digitalWrite(S2_LEFT, HIGH);
-	digitalWrite(S3_LEFT, HIGH);
-	// Read the output Pulse Width
-	RGB_LEFT[1] = pulseIn(OUT_LEFT, LOW);
-	
-  // Set sensor to read Blue only
-	digitalWrite(S2_LEFT,LOW);
-	digitalWrite(S3_LEFT,HIGH);
-	// Read the output Pulse Width
-	RGB_LEFT[2] = pulseIn(OUT_LEFT, LOW);
+  Dynamixel.wheel(SERVO_ID_1,RIGHT,0x3FF);              // Comman for Wheel mode, Move left at max speed
+  Dynamixel.wheel(SERVO_ID_2,LEFT,0x3FF);              // Comman for Wheel mode, Move left at max speed
 }
 
-void read_right_TCS3200()
+void stop_MX_28()
 {
-  // Set sensor to read Red only
-	digitalWrite(S2_RIGHT, LOW);
-	digitalWrite(S3_RIGHT, LOW);
-	// Read the output Pulse Width
-	RGB_RIGHT[0] = pulseIn(OUT_RIGHT, LOW);
-	
-
-  // Set sensor to read Green only
-	digitalWrite(S2_RIGHT, HIGH);
-	digitalWrite(S3_RIGHT, HIGH);
-	// Read the output Pulse Width
-	RGB_RIGHT[1] = pulseIn(OUT_RIGHT, LOW);
-	
-  // Set sensor to read Blue only
-	digitalWrite(S2_RIGHT,LOW);
-	digitalWrite(S3_RIGHT,HIGH);
-	// Read the output Pulse Width
-	RGB_RIGHT[2] = pulseIn(OUT_RIGHT, LOW);
+  Dynamixel.wheel(SERVO_ID_1,RIGHT,0x0);              // Comman for Wheel mode, Move left at max speed
+  Dynamixel.wheel(SERVO_ID_2,LEFT,0x0);              // Comman for Wheel mode, Move left at max speed
 }
 
-void command_MX_28(int rpm)
+void turn_left_MX_28()
 {
-  // Set Goal Velocity using RPM
-  dxl.setGoalVelocity(DXL_ID_1, rpm, UNIT_RPM);
-  dxl.setGoalVelocity(DXL_ID_2, rpm, UNIT_RPM);
+  Dynamixel.wheel(SERVO_ID_1,RIGHT,0x3FF);              // Comman for Wheel mode, Move left at max speed
+  Dynamixel.wheel(SERVO_ID_2,RIGHT,0x3FF);              // Comman for Wheel mode, Move left at max speed
+  delay(580);
+  stop_MX_28();
 }
 
-void print_MX_28_speed_rpm()
+void turn_right_MX_28()
 {
-  DEBUG_SERIAL.print("Present Velocity(rpm) : ");
-  DEBUG_SERIAL.println(dxl.getPresentVelocity(DXL_ID_1, UNIT_RPM));
-  DEBUG_SERIAL.println(dxl.getPresentVelocity(DXL_ID_2, UNIT_RPM));
+  Dynamixel.wheel(SERVO_ID_1,LEFT,0x3FF);              // Comman for Wheel mode, Move left at max speed
+  Dynamixel.wheel(SERVO_ID_2,LEFT,0x3FF);              // Comman for Wheel mode, Move left at max speed
+  delay(580);
+  stop_MX_28();
+}
+
+void left_to_right_demi_circle_MX_28()
+{
+
+}
+
+void clockwise_circle_MX()
+{
+  Dynamixel.wheel(SERVO_ID_1,LEFT,0x3FF);              // Comman for Wheel mode, Move left at max speed
+  delay(2000);
+  stop_MX_28();
 }
 
 int detect_obstacle()
 {
-  if (read_left_HC_SR04() < 75)
-  {
-    return 1;
-  }
   if (read_middle_HC_SR04() < 75)
   {
     return 2;
-  }
-  if (read_right_HC_SR04() < 75)
-  {
-    return 3;
   }
   return 0;
 }
@@ -202,102 +182,158 @@ void setup() {
   /*--------------*/
   /* Serial Debug */
   /*--------------*/
-  // Serial.begin(57600);
+  // Serial.begin(9600);
+
+  /*-------------*/
+  /* Setup MX-28 */
+  /*-------------*/
+  Dynamixel.begin(SERVO_SET_Baudrate);                                    // We now need to set Ardiuno to the new Baudrate speed
+  Dynamixel.setDirectionPin(SERVO_ControlPin);                            // Optional. Set direction control pin
+  Dynamixel.setMode(SERVO_ID_1, WHEEL, 0, 0);                             // set mode to WHEEL
+  Dynamixel.setMode(SERVO_ID_2, WHEEL, 0, 0);                             // set mode to WHEEL
 
   /*---------------*/
   /* Setup Tirette */
   /*---------------*/
-  DEBUG_SERIAL.begin(115200);
-  // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
-  dxl.begin(57600);
-  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
-  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
-  // Get DYNAMIXEL information
-  dxl.ping(DXL_ID_1);
-  dxl.ping(DXL_ID_2);
+  pinMode(TIRETTE_PIN, INPUT_PULLUP);
 
-  // Turn off torque when configuring items in EEPROM area
-  dxl.torqueOff(DXL_ID_1);
-  dxl.setOperatingMode(DXL_ID_1, OP_VELOCITY);
-  dxl.torqueOn(DXL_ID_1);
+  /*-----------------*/
+  /* Setup Interrupt */
+  /*-----------------*/
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
 
-  dxl.torqueOff(DXL_ID_2);
-  dxl.setOperatingMode(DXL_ID_2, OP_VELOCITY);
-  dxl.torqueOn(DXL_ID_2);
+  /*--------------------*/
+  /* Setup LEFT HC-SR04 */
+  /*--------------------*/
+  pinMode(TRIG_LEFT, OUTPUT);
+	pinMode(ECHO_LEFT, INPUT);
 
-  /*---------------*/
-  /* Setup Tirette */
-  /*---------------*/
-  // pinMode(TIRETTE_PIN, INPUT_PULLUP);
+  /*----------------------*/
+  /* Setup MIDDLE HC-SR04 */
+  /*----------------------*/
+  pinMode(TRIG_MIDDLE, OUTPUT);
+	pinMode(ECHO_MIDDLE, INPUT);
 
-  // /*--------------------*/
-  // /* Setup LEFT HC-SR04 */
-  // /*--------------------*/
-  // pinMode(TRIG_LEFT, OUTPUT);  
-	// pinMode(ECHO_LEFT, INPUT); 
+  /*---------------------*/
+  /* Setup RIGHT HC-SR04 */
+  /*---------------------*/
+  pinMode(TRIG_RIGHT, OUTPUT);
+	pinMode(ECHO_RIGHT, INPUT);
 
-  // /*----------------------*/
-  // /* Setup MIDDLE HC-SR04 */
-  // /*----------------------*/
-  // pinMode(TRIG_MIDDLE, OUTPUT);  
-	// pinMode(ECHO_MIDDLE, INPUT); 
-
-  // /*---------------------*/
-  // /* Setup RIGHT HC-SR04 */
-  // /*---------------------*/
-  // pinMode(TRIG_RIGHT, OUTPUT);  
-	// pinMode(ECHO_RIGHT, INPUT);
-
-  // /*--------------------*/
-  // /* Setup LEFT TCS3200 */
-  // /*--------------------*/
-  // // Set S0 - S3 as outputs
-	// pinMode(S0_LEFT, OUTPUT);
-	// pinMode(S1_LEFT, OUTPUT);
-	// pinMode(S2_LEFT, OUTPUT);
-	// pinMode(S3_LEFT, OUTPUT);
-
-	// // Set Pulse Width scaling to 20%
-	// digitalWrite(S0_LEFT, HIGH);
-	// digitalWrite(S1_LEFT, LOW);
-	
-  // // Set Sensor output as input
-	// pinMode(OUT_LEFT, INPUT);
-
-  // /*---------------------*/
-  // /* Setup RiGHT TCS3200 */
-  // /*---------------------*/
-  // // Set S0 - S3 as outputs
-	// pinMode(S0_RIGHT, OUTPUT);
-	// pinMode(S1_RIGHT, OUTPUT);
-	// pinMode(S2_RIGHT, OUTPUT);
-	// pinMode(S3_RIGHT, OUTPUT);
-
-	// // Set Pulse Width scaling to 20%
-	// digitalWrite(S0_RIGHT, HIGH);
-	// digitalWrite(S1_RIGHT, LOW);
-	
-  // // Set Sensor output as input
-	// pinMode(OUT_RIGHT, INPUT);
+  /*------------*/
+  /* Setup LEDs */
+  /*------------*/
+  pinMode(redPin,  OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
   setup();
 
-  while (1) {
-    // Serial.print("Distance Right : ");
-    // Serial.println(read_right_HC_SR04());
-    // read_right_TCS3200();
-    // Serial.print("Red : ");
-    // Serial.print(RGB_RIGHT[0]);
-    // Serial.print(", ");
-    // Serial.print("Green : ");
-    // Serial.print(RGB_RIGHT[1]);
-    // Serial.print(", ");
-    // Serial.print("Blue : ");
-    // Serial.println(RGB_RIGHT[2]);
-    command_MX_28(10);
-    print_MX_28_speed_rpm();
-    delay(100);
-  }  
+  // go_straight_MX_28();
+
+  // while(1)
+  // {
+  //   go_straight_MX_28();
+  //   delay(10000);
+  //   turn_left_MX_28();
+  //   go_straight_MX_28();
+  //   delay(2000);
+  //   stop_MX_28();
+  //   delay(60000);
+  // }
+
+  // delay(10000);
+
+  // unsigned long duration_1 = 0;
+  // unsigned long duration_2 = 0;
+
+  while (digitalRead(TIRETTE_PIN) == 1) {}
+
+  // delay(85000);
+
+  unsigned long start_time_1 = millis();
+  
+  while (millis() - start_time_1 < 9700)
+  {
+    if (detect_obstacle() != 0)
+    {
+      stop_MX_28();
+    }
+    else {
+      go_straight_MX_28();
+    }
+    while (millis() - start_time_1 > 15000)
+    {
+      servo.attach(10);
+      while (1)
+      {
+        stop_MX_28();
+        move_servo();
+        light_LEDs();
+      }
+    }
+  }
+
+  stop_MX_28();
+  // If Switch on 0 Left, if on 1 Right
+  if (digitalRead(INTERRUPT_PIN))
+  {
+    turn_left_MX_28();
+  }
+  else
+  {
+    turn_right_MX_28();
+  }
+  stop_MX_28();
+
+  unsigned long start_time_2 = millis();
+
+  while (millis() - start_time_2 < 2300)
+  {
+    if (detect_obstacle() != 0)
+    {
+      stop_MX_28();
+    }
+    else {
+      go_straight_MX_28();
+    }
+    while (millis() - start_time_1 > 15000)
+    {
+      servo.attach(10);
+      while (1)
+      {
+        stop_MX_28();
+        move_servo();
+        light_LEDs();
+      }
+    }
+  }
+
+  stop_MX_28();
+
+  /*-------------*/
+  /* Setup Servo */
+  /*-------------*/
+  servo.attach(10);
+
+  while(1) {
+    stop_MX_28();
+    move_servo();
+    light_LEDs();
+
+    // if (detect_obstacle() != 0)
+    // {
+    //   stop_MX_28();
+    // }
+    // else
+    // {
+    //   go_straight_MX_28();
+    // }
+
+    // Serial.print("Distance : ");
+    // Serial.println(read_middle_HC_SR04());
+    // delay(100);
+  }
 }
